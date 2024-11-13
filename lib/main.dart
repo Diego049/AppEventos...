faf2d3694e47188f92f9263171c3dbc443b98ba6
Diego <diego.llamas@upnay.edu.mx>
2024-11-13 11:26:09 -0600

    se agg el main

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://tu-supabase-url.supabase.co', // Cambia esta URL por la tuya
-    anonKey: 'tu-anon-key', // Cambia este anonKey por el tuyo
+    url: 'https://srqagkrqaqztlpxrfduz.supabase.co', // Cambia esta URL por la tuya
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNycWFna3JxYXF6dGxweHJmZHV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzNDEyNTAsImV4cCI6MjA0NjkxNzI1MH0.-4VpRoRKuWvaKxVEL40ci4DVEdUhGPrMpOmI4j3-_XQ', // Cambia este anonKey por el tuyo
   );
 
   runApp(MyApp());
